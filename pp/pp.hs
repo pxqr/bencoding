a6b54241f2a5c56ec9b5103f34688bd26e69f810
Sam T <sta.cs.vsu@gmail.com>
2013-03-31 15:46:21 +0400

    add test sample program - pretty print of bencoded file

---
+++ pp/pp.hs
@@ -0,0 +1,14 @@
+module Main (main) where
+
+import Data.BEncode
+import qualified Data.ByteString as B
+import System.IO
+import System.Environment
+
+main :: IO ()
+main = do
+  path : _ <- getArgs
+  content  <- B.readFile path
+  case decode content of
+    Left e -> hPutStrLn stderr e
+    Right be -> printPretty be
